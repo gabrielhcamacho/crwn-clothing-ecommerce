7be9984c00ffdb3a8bbac2f9123e44544110d43d
Gabriel Camacho <gabrielh.camacho@outlook.com>
2022-02-09 16:35:15 -0300

    separating components, finished styling menu-item

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+import Directory from '../../components/directory/Directory';
+
+const HomePage = () => (
+  <div classNAme="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
